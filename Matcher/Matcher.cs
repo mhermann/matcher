0c93a6a08cc6f6d7acd51d41f565a7ed8fc90379
mathiashermann <mh@mathiashermann.dk>
2013-10-26 18:16:35 +0200

    First

---
+++ Matcher/Matcher.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Matcher
+{
+    public static class Matcher
+    {
+        //public static IEnumerable<string> LowercaseUsernames(string input, int minCharacters, int maxCharacters)
+        //{
+        //    var regEx = "^[a-z0-9_-]{3,16}$";
+        //    regEx = regEx.Replace("3", minCharacters.ToString());
+        //    regEx = regEx.Replace("16", maxCharacters.ToString());
+        //    var matches = Regex.Matches(input, regEx, RegexOptions.IgnoreCase);
+        //    var matchedStrings = new List<string>();
+        //    for (int i = 0; i < matches.Count; i++)
+        //    {
+        //        matchedStrings.Add(matches[i].Value);
+        //    }
+        //    return matchedStrings;
+        //}
+
+        public static IEnumerable<string> Urls(string input)
+        {
+            var matches = Regex.Matches(input, @"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)",
+                                      RegexOptions.IgnoreCase);
+            var matchedStrings = new List<string>();
+            for (int i = 0; i < matches.Count; i++)
+            {
+                matchedStrings.Add(matches[i].Value);
+            }
+            return matchedStrings;
+        }
+
+        public static IEnumerable<string> Emails(string input)
+        {
+            var matches = Regex.Matches(input, @"(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})",
+                                      RegexOptions.IgnoreCase);
+            var matchedStrings = new List<string>();
+            for (int i = 0; i < matches.Count; i++)
+            {
+                matchedStrings.Add(matches[i].Value);
+            }
+            return matchedStrings;
+        }
+
+        public static IEnumerable<string> Hashtags(string input)
+        {
+            var matches = Regex.Matches(input, @"(^|[^0-9A-Z&/]+)(#|\uFF03)([0-9A-Z_]*[A-Z_]+[a-z0-9_\\u00c0-\\u00d6\\u00d8-\\u00f6\\u00f8-\\u00ff]*)",
+                                      RegexOptions.IgnoreCase);
+            var matchedStrings = new List<string>();
+            for (int i = 0; i < matches.Count; i++)
+            {
+                matchedStrings.Add(matches[i].Value);
+            }
+            return matchedStrings;
+        } 
+    }
+}
---
+++ MatcherTests/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matcher;
+namespace MatcherTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //[TestMethod]
+        //public void TestLowercaseUsername()
+        //{
+        //    Assert.IsTrue(Matcher.Matcher.LowercaseUsernames("skinke", 3, 16).Any());
+        //    Assert.IsTrue(!Matcher.Matcher.LowercaseUsernames("Sk as as", 3, 16).Any());
+        //    Assert.IsTrue(Matcher.Matcher.LowercaseUsernames("skinke, skinke, tu", 3, 16).Count() == 2);
+        //    Assert.IsTrue(!Matcher.Matcher.LowercaseUsernames("mathias", 4, 6).Any());
+        //}
+
+        [TestMethod]
+        public void TestIncorrectUrl()
+        {
+            Assert.IsTrue(!Matcher.Matcher.Urls("skinke").Any());
+        }
+
+        [TestMethod]
+        public void TestTwoIncorrectUrl()
+        {
+            Assert.IsTrue(!Matcher.Matcher.Urls("skinke ww.te").Any());
+        }
+
+        [TestMethod]
+        public void TestCorrectUrlWithHttp()
+        {
+            Assert.IsTrue(Matcher.Matcher.Urls("http://lyn.dk").Any());
+        }
+
+        [TestMethod]
+        public void TestTwoCorrectUrlWithHttp()
+        {
+            Assert.IsTrue(Matcher.Matcher.Urls("http://lyn.dk http://fiske.dk").Count() == 2);
+        }
+
+        [TestMethod]
+        public void TestIncorrectEmail()
+        {
+            Assert.IsTrue(!Matcher.Matcher.Emails("http://lyn.dk").Any());
+        }
+
+        [TestMethod]
+        public void TestCorrectEmail()
+        {
+            Assert.IsTrue(Matcher.Matcher.Emails("fisk@dr.dk").Any());
+        }
+
+        [TestMethod]
+        public void TestCorrectHashtags()
+        {
+            Assert.IsTrue(Matcher.Matcher.Emails("fisk@dr.dk #test").Any());
+        }
+
+        [TestMethod]
+        public void TestCorrectHashtagsWithHyphen()
+        {
+            Assert.IsTrue(Matcher.Matcher.Emails("fisk@dr.dk #test-igen").Any());
+        }
+
+        [TestMethod]
+        public void TestTwoCorrectHashtags()
+        {
+            Assert.IsTrue(Matcher.Matcher.Emails("fisk@dr.dk #test #lun").Any());
+        }
+    }
+}
